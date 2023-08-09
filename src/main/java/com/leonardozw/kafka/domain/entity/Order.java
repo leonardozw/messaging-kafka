f0ccceb055a9f592aa363829306187c3d7772722
leonardo <leonardozw46@gmail.com>
2023-08-09 18:29:11 -0300

    feat: Order entity created

---
+++ src/main/java/com/leonardozw/kafka/domain/entity/Order.java
@@ -0,0 +1,29 @@
+package com.leonardozw.kafka.domain.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderItem;
+    private LocalDateTime orderDate;
+    private Double orderPrice;
+    private String orderAdress;
+    private String orderPayment;
+}
